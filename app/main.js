443cba4cb5da84bec34d45284f0137bade1d0d9e
sanemat <o.gata.ken@gmail.com>
2013-07-28 19:43:49 +0900

    Use getAttribute instead of dataset, for IE

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 /*jshint strict:false, browser:true */
 (function bookmarklet() {
-  var sku = document.body.querySelector('#content .images').dataset.omgProductSku;
+  var sku = document.body.querySelector('#content .images').getAttribute('data-omg-product-sku');
 
   window.prompt('Copy to clipboard: ⌘+C or Ctrl+C then Enter', sku);
 }());
