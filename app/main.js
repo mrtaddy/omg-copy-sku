8fa9292f8ece751ff2a0ca593aa2f842f9c26d49
sanemat <o.gata.ken@gmail.com>
2013-07-28 23:39:15 +0900

    Use jQuery in site

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 /*jshint strict:false, browser:true */
 (function bookmarklet() {
-  var sku = document.body.querySelector('#content .images').getAttribute('data-omg-product-sku');
+  var sku = $('#content .images').data('omgProductSku');
 
   window.prompt('Copy to clipboard: ⌘+C or Ctrl+C then Enter', sku);
 }());
