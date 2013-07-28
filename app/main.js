7d8fb01bad6378cf5b7a4db44ed7290e498ca476
sanemat <o.gata.ken@gmail.com>
2013-07-29 00:23:27 +0900

    Add last semi column

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 /*jshint strict:false, browser:true */
 (function bookmarklet() {
   var sku = $('#content .images').data('omgProductSku');
-  var information = {sku: sku}
+  var information = {sku: sku};
 
   alert(JSON.stringify(information, null, 2));
 }());
