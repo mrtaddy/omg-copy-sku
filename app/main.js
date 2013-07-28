37a2ef8490708e46d1395280591e8efd615b2345
sanemat <o.gata.ken@gmail.com>
2013-07-29 01:59:00 +0900

    Use strip method

--- app/main.js
+++ app/main.js
@@ -5,18 +5,18 @@
   var url = $('meta[rel="canonical"]').attr('href');
   var permalink = url.match(/\/([^/]*)$/)[1];
   var properties = $('#product-properties');
-  var brand = $(properties.find('dd')[0]).find('a').text().trim();
-  var color = $(properties.find('dd')[1]).text().trim();
-  var lenswidth = $(properties.find('dd')[2]).text().trim().replace(/[\n\r]/g, '');
-  var lensheight = $(properties.find('dd')[3]).text().trim().replace(/[\n\r]/g, '');
-  var bridgewidth = $(properties.find('dd')[4]).text().trim().replace(/[\n\r]/g, '');
-  var templewidth = $(properties.find('dd')[5]).text().trim().replace(/[\n\r]/g, '');
-  var shape = $(properties.find('dd')[6]).text().trim();
-  var rimshape = $(properties.find('dd')[7]).text().trim();
-  var material = $(properties.find('dd')[8]).text().trim();
-  var temple_material = $(properties.find('dd')[9]).text().trim();
-  var nosepad = $(properties.find('dd')[10]).text().trim();
-  var fitting = $(properties.find('dd')[11]).text().trim();
+  var brand = strip($(properties.find('dd')[0]).text());
+  var color = strip($(properties.find('dd')[1]).text());
+  var lenswidth = strip($(properties.find('dd')[2]).text());
+  var lensheight = strip($(properties.find('dd')[3]).text());
+  var bridgewidth = strip($(properties.find('dd')[4]).text());
+  var templewidth = strip($(properties.find('dd')[5]).text());
+  var shape = strip($(properties.find('dd')[6]).text());
+  var rimshape = strip($(properties.find('dd')[7]).text());
+  var material = strip($(properties.find('dd')[8]).text());
+  var temple_material = strip($(properties.find('dd')[9]).text());
+  var nosepad = strip($(properties.find('dd')[10]).text());
+  var fitting = strip($(properties.find('dd')[11]).text());
 
   var information = { name: name,
     sku: sku,
@@ -37,4 +37,8 @@
   };
 
   alert(JSON.stringify(information, null, 2));
+
+  function strip(string) {
+    return string.trim().replace(/[\n\r]/g, '');
+  }
 }());
